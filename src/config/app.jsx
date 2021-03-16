5e6048a17aa573673e0adf991d86185759a628b2
Rico Sandyca <ricosandyca.if@gmail.com>
2021-03-16 17:06:39 +0700

    Add config data

---
+++ src/config/app.jsx
@@ -0,0 +1,5 @@
+export default app = {
+  appName: process.env.REACT_APP_NAME || 'Spotify Web API',
+  appURL: process.env.REACT_APP_URL || 'http://localhost:3000',
+  spotifyClientId: process.env.REACT_APP_SPOTIFY_CLIENT_ID || ''
+}
