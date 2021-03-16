b42a7b322b7a0343f49b6c0235f1ca4e5b8f2a5b
Rico Sandyca <ricosandyca.if@gmail.com>
2021-03-16 20:23:27 +0700

    Add config attribute

--- src/config/app.js
+++ src/config/app.js
@@ -1,7 +1,8 @@
 const app = {
   appName: process.env.REACT_APP_NAME || 'Spotify Web API',
   appURL: process.env.REACT_APP_URL || 'http://localhost:3000',
-  spotifyClientId: process.env.REACT_APP_SPOTIFY_CLIENT_ID || ''
+  spotifyClientId: process.env.REACT_APP_SPOTIFY_CLIENT_ID || '',
+  spotifyAPIURL: process.env.REACT_APP_SPOTIFY_API_URL || 'https://api.spotify.com/v1'
 }
 
 
---
+++ src/hooks/useUser.js
@@ -0,0 +1,7 @@
+export function useSetUserData() {
+
+}
+
+export function useGetUserData() {
+  
+}
